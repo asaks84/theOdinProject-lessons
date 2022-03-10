718f62b9f4420e72f5d95ff6ca243318bdb23746
caue <cauecpl@gmail.com>
2022-03-10 15:57:42 -0300

    5 first exs solved

--- odin-exs-list/05_sumAll/sumAll.js
+++ odin-exs-list/05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const num1 = a;
+    const num2 = b;
+    let sum = num1+num2;
 
+    if( typeof(sum) !== 'number' || num1<0 || num2<0)   { return "ERROR"; }
+    if(num1>num2){
+        for(i=num1-num2;i>num2;i--){
+            sum += i;
+        }
+    }
+    for(i=num2-num1;i>num1;i--){
+        sum += i;
+    }
+    return sum;        
 };
 
 // Do not edit below this line
