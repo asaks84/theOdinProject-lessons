431539e32002a48bdd4ee187d2db6b7e6561a321
caue <cauecpl@gmail.com>
2022-03-10 18:11:03 -0300

    excercises 6 n 7

--- odin-exs-list/06_leapYears/leapYears.js
+++ odin-exs-list/06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function (a) {
+    const yearTest = a;
 
+    if (yearTest % 4 == 0 && yearTest % 100 != 0 ||
+        yearTest % 100 == 0 && yearTest % 400 == 0) {
+        return true;
+    };
+    return false;
+
+    /*
+    if(yearTest%4 == 0){
+        if(yearTest%100 == 0){
+            if(yearTest%400 == 0) {
+                return true;
+            };
+            return false;
+        };
+        return true;
+    };
+    return false;
+    */
 };
 
 // Do not edit below this line
--- odin-exs-list/07_tempConversion/tempConversion.js
+++ odin-exs-list/07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let result = (f-32)*(5/9);
+  if(Number.isInteger(result) != true){ result = parseFloat(result.toFixed(1)); }
+  return result;
 };
 
-const ctof = function() {
+const ctof = function(c) {
 
+  let result = (9/5) * c + 32;
+  if(Number.isInteger(result) != true){ result = parseFloat(result.toFixed(1)); }
+  return result;
 };
 
 // Do not edit below this line
