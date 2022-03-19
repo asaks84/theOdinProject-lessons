3af3c4b9f688c7be5b235edf0071f9ceeb13c0ee
caue <cauecpl@gmail.com>
2022-03-19 20:17:54 -0300

    New files, most of them are images to the game layout. Opening transition created.

--- projects/rockPaperScissor/js/script.js
+++ projects/rockPaperScissor/js/script.js
@@ -4,28 +4,29 @@ let roundResult = '';
 let roundCounter = 1;
 
 const buttons = document.querySelectorAll('.choiceBtn');
+const butonStart = document.querySelector('button.play');
 
-function isTheWinner(){
-    if(pcPoints > playerPoints) {
+function isTheWinner() {
+    if (pcPoints > playerPoints) {
         console.log('%c SORRY, YOU LOST!', 'color:red;');
-    } else if(pcPoints < playerPoints) {
+    } else if (pcPoints < playerPoints) {
         console.log('%c NICE! YOU WON THE GAME!', 'color: green;');
     };
 };
-function gameOver(){
-    if(pcPoints == 5 || playerPoints == 5){
+function gameOver() {
+    if (pcPoints == 5 || playerPoints == 5) {
         console.error('THE GAME IS FUCKIN OVER!');
         buttons.forEach(button => button.setAttribute("disabled", "true"));
         isTheWinner();
     };
 };
-function capitalyze(str){
+function capitalyze(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 function computerPlay() {
     const randomNum = Math.floor(Math.random() * 3);
-    
-    switch(randomNum){
+
+    switch (randomNum) {
         case 0: return 'ROCK';
         case 1: return 'PAPER';
         case 2: return 'SCISSOR';
@@ -36,19 +37,19 @@ function playRound(playerSelection) {
     const playerMove = playerSelection.target.value.toUpperCase();
 
     console.warn(`Round: %c${roundCounter}`, 'color: red;')
-   
+
     // setting the round result
     if (pcMove == playerMove) {
-        roundResult ="It's a Tie!";
+        roundResult = "It's a Tie!";
     } else if (
         (pcMove == 'ROCK' && playerMove == 'SCISSOR') ||
         (pcMove == 'PAPER' && playerMove == 'ROCK') ||
         (pcMove == 'SCISSOR' && playerMove == 'PAPER')) {
-            ++pcPoints;
-            roundResult = "You Lose!";
+        ++pcPoints;
+        roundResult = "You Lose!";
     } else {
         ++playerPoints;
-        roundResult ="You Win!";
+        roundResult = "You Win!";
     };
 
     //printing round results
@@ -59,13 +60,37 @@ function playRound(playerSelection) {
     ++roundCounter;
     gameOver();
 };
-function controls(e){
-   const control = document.querySelector(`button[data-key="${e.keyCode}"]`);
-   if(!control) return;
-   control.click();   
+function controls(e) {
+    const control = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    if (!control) return;
+    control.click();
 }
 
 // UI
+function startGame() {
+    const gameStart = Array.from(document.querySelectorAll('.opening'));
+    const divContainer = document.querySelector('div#container');
+    const gameOpening = Array.from(document.querySelectorAll('.displayNone'));
 
+    gameStart.forEach(disable => {
+        disable.classList.add('hidden');
+        divContainer.classList.add('bgOut');
+        setTimeout(function () {
+            disable.classList.add('displayNone');
+        }, 350);
+    });
+
+    gameOpening.forEach(enable => {
+        enable.classList.remove('displayNone');
+        setTimeout(function () {
+            enable.classList.remove('hidden');
+            enable.classList.add('visible');
+        }, 500);
+    });
+
+}
 buttons.forEach(button => button.addEventListener('click', playRound));
-window.addEventListener('keydown', controls)
+
+// start the game
+addEventListener('click', startGame);
+window.addEventListener('keydown', controls);
