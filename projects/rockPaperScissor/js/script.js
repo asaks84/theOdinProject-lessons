d3acef3a37a55360956d465b2bb2d5d83a417dc5
caue <cauecpl@gmail.com>
2022-03-19 02:25:17 -0300

    First step to UI dev.

--- projects/rockPaperScissor/js/script.js
+++ projects/rockPaperScissor/js/script.js
@@ -3,39 +3,41 @@ let playerPoints = 0;
 let roundResult = '';
 let roundCounter = 1;
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.choiceBtn');
 
 function isTheWinner(){
     if(pcPoints > playerPoints) {
-        console.error('You lost the game!');
+        console.log('%c SORRY, YOU LOST!', 'color:red;');
     } else if(pcPoints < playerPoints) {
-        console.warn('You won the game!');
+        console.log('%c NICE! YOU WON THE GAME!', 'color: green;');
     };
 };
 function gameOver(){
-    if(pcPoints == 3 || playerPoints == 3){
+    if(pcPoints == 5 || playerPoints == 5){
         console.error('THE GAME IS FUCKIN OVER!');
         buttons.forEach(button => button.setAttribute("disabled", "true"));
         isTheWinner();
     };
 };
+function capitalyze(str){
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
 function computerPlay() {
     const randomNum = Math.floor(Math.random() * 3);
-    let pcSelect;
-
-    if (randomNum == 1) {
-        pcSelect = 'Rock';
-    } else if (randomNum == 2) {
-        pcSelect = 'Paper';
-    } else pcSelect = 'Scissor';
-    return pcSelect.toUpperCase();
+    
+    switch(randomNum){
+        case 0: return 'ROCK';
+        case 1: return 'PAPER';
+        case 2: return 'SCISSOR';
+    };
 };
 function playRound(playerSelection) {
     const pcMove = computerPlay();
     const playerMove = playerSelection.target.value.toUpperCase();
 
-    console.warn(`Round: ${roundCounter}`)
-    // creating the conditions
+    console.warn(`Round: %c${roundCounter}`, 'color: red;')
+   
+    // setting the round result
     if (pcMove == playerMove) {
         roundResult ="It's a Tie!";
     } else if (
@@ -43,19 +45,27 @@ function playRound(playerSelection) {
         (pcMove == 'PAPER' && playerMove == 'ROCK') ||
         (pcMove == 'SCISSOR' && playerMove == 'PAPER')) {
             ++pcPoints;
-            roundResult ="You Lose!";
+            roundResult = "You Lose!";
     } else {
         ++playerPoints;
         roundResult ="You Win!";
-    } console.log(roundResult);
+    };
 
-    console.log(`PC: ${pcMove}`);
-    console.log(`YOU: ${playerMove}`);
-    console.log(`POINTS:\nPC: ${pcPoints}\nUSER: ${playerPoints}`);
+    //printing round results
+    console.log(`${capitalyze('You')}: ${capitalyze(playerMove)}`);
+    console.log(`Pc: ${capitalyze(pcMove)}`);
+    console.log(`Points:\nPc: ${pcPoints}\nYou: ${playerPoints}`);
+    console.log(`%c${roundResult.toUpperCase()}`, 'color: white;');
     ++roundCounter;
     gameOver();
 };
+function controls(e){
+   const control = document.querySelector(`button[data-key="${e.keyCode}"]`);
+   if(!control) return;
+   control.click();   
+}
 
 // UI
 
 buttons.forEach(button => button.addEventListener('click', playRound));
+window.addEventListener('keydown', controls)
