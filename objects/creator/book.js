b698801daf979c3cdc30dc47abdcb638671f9d90
caue <cauecpl@gmail.com>
2022-05-23 17:07:39 -0300

    1st study of Object Creator

---
+++ objects/creator/book.js
@@ -0,0 +1,24 @@
+function book(bookName, bookAuthor, bookPages) {
+    this.name = bookName,
+    this.author = bookAuthor,
+    this.pages = bookPages
+    
+    this.info = ()  => `${this.name} by ${this.author} actualy have ${this.pages} pages.`;
+
+};
+
+// const tempName = this.name;
+//     function capitalize (aString) {
+//         const toArray = aString.toLowerCase().split(" ");
+        
+//         for(i = 1; i < toArray.length; i++){
+//             toArray[i] =  toArray[i].charAt(0).toUpperCase() + toArray[i].slice(1);
+//         }
+//         const capitalized = toArray.join(" ");
+//         return capitalized;
+//     }
+//     const punctRemove = /[^A-Za-z]/g;
+//     const propertyName = capitalize(tempName).replace(punctRemove, "");
+
+const oHotdogGostoso = new book("O hotdog gostoso", "Eu mermo", 300);
+console.log(oHotdogGostoso.info());
