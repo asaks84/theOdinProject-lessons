1fd0f4f468d22f7e5e89c1f3d480cec36758feaf
caue <cauecpl@gmail.com>
2022-05-24 14:29:47 -0300

    Testing Object.create and don't understanding it.

--- objects/creator/book.js
+++ objects/creator/book.js
@@ -1,27 +1,26 @@
+function ShowProperties(obj) {
+    for (const prop in obj) {
+        console.log(prop + ": " + obj[prop] + "\n");
+    };
+};
+
 function book(bookName, bookAuthor, bookPages) {
     this.name = bookName,
     this.author = bookAuthor,
-    this.pages = bookPages
+    this.pages = bookPages; 
 };
 
-book.prototype.info = function() { 
+book.prototype.info = function () {
     return `${this.name} by ${this.author} actualy have ${this.pages} pages.`;
-}
-
-
-// const tempName = this.name;
-//     function capitalize (aString) {
-//         const toArray = aString.toLowerCase().split(" ");
-        
-//         for(i = 1; i < toArray.length; i++){
-//             toArray[i] =  toArray[i].charAt(0).toUpperCase() + toArray[i].slice(1);
-//         }
-//         const capitalized = toArray.join(" ");
-//         return capitalized;
-//     }
-//     const punctRemove = /[^A-Za-z]/g;
-//     const propertyName = capitalize(tempName).replace(punctRemove, "");
+};
 
 const oHotdogGostoso = new book("O hotdog gostoso", "Eu mermo", 300);
+const theHobbit = Object.create(book)
+
+theHobbit.author = "J. R. R. Tolkien";
+theHobbit['name'] = 'The Hobbit';
+theHobbit['pages'] = 295;
 
 console.log(oHotdogGostoso.info());
+// console.log(theHobbit.info());
+ShowProperties(theHobbit);
