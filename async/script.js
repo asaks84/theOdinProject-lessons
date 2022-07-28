45ceb70f599ce17b0e204cf9374e231f13637037
Cauê Carrera <cauecpl@gmail.com>
2022-07-28 11:41:42 -0300

    Search for giphy

--- async/script.js
+++ async/script.js
@@ -1,25 +1,24 @@
-const body = document.querySelector('body');
-const img = document.createElement('img');
+const body = document.querySelector("body");
+const img = document.createElement("img");
 
-async function getGiphy(word){
-  try{
-    const url = 'https://api.giphy.com/v1/gifs/';
-    const type = 'translate?';
-    const key = 'api_key=G5oNi5lR0E7gMlKDhAyudh2dv074xuCG';
-    const getWord = '&s='+word;
-    const mode = { mode: 'cors' };
-    
-    const response = await fetch(url+type+key+getWord, mode);
+async function getGiphy(word) {
+  try {
+    const url = "https://api.giphy.com/v1/gifs/";
+    const type = "translate?";
+    const key = "api_key=G5oNi5lR0E7gMlKDhAyudh2dv074xuCG";
+    const getWord = "&s=" + word;
+    const mode = { mode: "cors" };
+
+    const response = await fetch(url + type + key + getWord, mode);
     const catData = await response.json();
     return catData.data.images.original.url;
-  } catch(error){
+  } catch (error) {
     console.log(error);
   }
 }
 
-async function newcat(){
- img.src = await getGiphy('cats'); 
- body.appendChild(img);
+async function newcat() {
+  const value = document.querySelector("#data").value;
+  img.src = await getGiphy(value);
+  body.appendChild(img);
 }
-
-newcat()
